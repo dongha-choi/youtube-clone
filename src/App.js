7cc64d7b3789bbca8dedeb400dba8bd1f064394c
Dongha Choi <carmania0701@gmail.com>
2024-05-08 21:26:24 +0900

    Correction of structural errors
    Router was placed in index.js, so it is now moved to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import Home from './pages/Home';
+import Root from './pages/Root';
+import NotFound from './pages/NotFound';
+import Videos from './pages/Videos';
+import Watch from './pages/Watch';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Videos /> },
+      {
+        path: '/videos/:q',
+        element: <Videos />, //pass props of search query
+      },
+      {
+        path: '/videos/watch/:videoId',
+        element: <Watch />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,35 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-// import Home from './pages/Home';
-import Root from './pages/Root';
-import NotFound from './pages/NotFound';
-import Videos from './pages/Videos';
-import Watch from './pages/Watch';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Videos /> },
-      {
-        path: '/videos/:q',
-        element: <Videos />, //pass props of search query
-      },
-      {
-        path: '/videos/watch/:videoId',
-        element: <Watch />, //pass props of search query
-      },
-    ],
-  },
-]);
+
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
