1a912a2cc015a471985d82f5eb1afc6f1803325d
Dongha Choi <carmania0701@gmail.com>
2024-05-08 21:31:17 +0900

    Navbar form

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 
 export default function Navbar() {
+  const [text, setText] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setText('');
+    navigate(`/videos/${text}`);
+  };
+  const handleChange = (e) => setText(e.target.value);
   return (
     <div>
-      <p>logo</p>
-      <form action=''></form>
+      <Link to='/'>Logo</Link>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={text}
+          placeholder='Search...'
+          onChange={handleChange}
+        />
+        <button>🔍</button>
+      </form>
     </div>
   );
 }
